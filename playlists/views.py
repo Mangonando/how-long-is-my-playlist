aaa37122a66473cdf1dd75b0ec66a316907a1e7e
Mangonando <juan.f.ramirezv@gmail.com>
2026-06-19 13:56:02 +0200

    Add ui with url routing, calculator and html template

--- playlists/views.py
+++ playlists/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from project import (
+    calculate_total_seconds,
+    calculate_adjusted_duration,
+    format_duration,
+)
+
+def calculator(request):
+    result = None
+    
+    if request.method == "POST":
+        hours = int(request.POST.get("hours", 0))
+        minutes = int(request.POST.get("minutes", 0))
+        seconds = int(request.POST.get("seconds", 0))
+        song_count = int(request.POST.get("song_count", 0))
+        crossfade_seconds = int(request.POST.get("crossfade_seconds", 0))
+
+        total_seconds = calculate_total_seconds(hours, minutes, seconds)
+        adjusted_seconds = calculate_adjusted_duration(total_seconds, song_count, crossfade_seconds)
+
+        result = format_duration(adjusted_seconds)
+
+    return render(request, "playlists/calculator.html", {
+        "result": result
+    })
